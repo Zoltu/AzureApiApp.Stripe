85911be7e5338145889be9bdb13dd3ff58d11dc2
Micah Zoltu <micah@zoltu.net>
2015-09-06 12:04:10 -0700

    Adds CreateCustomer.

--- apiapp/Controllers/StripeController.cs
+++ apiapp/Controllers/StripeController.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics.Contracts;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace Zoltu.AzureApiApp.Controllers
@@ -7,12 +12,38 @@ namespace Zoltu.AzureApiApp.Controllers
 	public class StripeController : ApiController
 	{
 		[HttpGet]
-		[Route("api/Stripe")]
-		public String Get()
+		[Route("api/CreateCustomer")]
+		public async Task<String> CreateCustomer(String cardTokenId, String stripeApiKey)
 		{
+			Contract.Requires(cardTokenId != null);
+			Contract.Requires(stripeApiKey != null);
 			Contract.Ensures(Contract.Result<String>() != null);
+			Contract.Ensures(Contract.Result<Task<String>>() != null);
 
-			return "Hello API App!";
+			var address = @"https://api.stripe.com/v1/customers";
+			var authorizationHeader = GetBasicAuthenticationHeader(stripeApiKey, "");
+			var content = new Dictionary<String, String> { { "source", cardTokenId } };
+			var formContent = new FormUrlEncodedContent(content);
+
+			var httpClient = new HttpClient();
+			httpClient.DefaultRequestHeaders.Authorization = authorizationHeader;
+			var result = await httpClient.PostAsync(address, formContent);
+			if (!result.IsSuccessStatusCode)
+				throw new HttpResponseException(result);
+
+			return await result.Content.ReadAsStringAsync();
+		}
+
+		private static AuthenticationHeaderValue GetBasicAuthenticationHeader(String username, String password)
+		{
+			Contract.Requires(username != null);
+			Contract.Requires(password != null);
+			Contract.Ensures(Contract.Result<AuthenticationHeaderValue>() != null);
+
+			var content = $"{username}:{password}";
+			var contentBytes = Encoding.ASCII.GetBytes(content);
+			var contentBytesEncoded = Convert.ToBase64String(contentBytes);
+			return new AuthenticationHeaderValue("Basic", contentBytesEncoded);
 		}
 	}
 }
